464c89fe7a5a86f12be2074da883f9a9daea472a
Calderonraul <raulcalderon@ravn.co>
2022-06-10 18:34:06 -0600

    :construction: Paging still work in progress

--- app/src/main/java/com/example/starwarsapigraphql/presentation/PersonViewModel.kt
+++ app/src/main/java/com/example/starwarsapigraphql/presentation/PersonViewModel.kt
@@ -39,7 +39,6 @@ class PersonViewModel @Inject constructor(
 
     val pagedInfo: Flow<PagingData<PersonDomain>> = Pager(PagingConfig(pageSize = 5)){
         personsSource
-
     }.flow.cachedIn(viewModelScope)
 
 
